62d050f23948474f695f26b462022d6947d4a25a
GochenRyan <rangaocheng@outlook.com>
2023-06-13 19:29:11 +0800

    Update Clean.bat

--- Clean.bat
+++ Clean.bat
@@ -1,2 +1,23 @@
 rmdir /s/q build
 rmdir /s/q lib
+rmdir /s/q output
+
+set VENDOR_PATH=%cd%\\Vendor
+
+rem clean freetype
+cd /d %VENDOR_PATH%\\freetype
+cmake -E remove_directory build
+cmake -E remove_directory lib
+
+rem clean SDL
+cd /d %VENDOR_PATH%\\SDL
+cmake -E remove_directory build
+cmake -E remove_directory lib
+
+rem clean spdlog
+cd /d %VENDOR_PATH%\\spdlog
+cmake -E remove_directory build
+cmake -E remove_directory lib
+
+
+
