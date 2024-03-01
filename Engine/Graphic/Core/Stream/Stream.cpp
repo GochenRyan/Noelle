8ba5fdfcb0127ff456987110c70e6e87faa7aee9
Gaocheng <rangaocheng@outlook.com>
2024-03-01 16:17:14 +0800

    New Year!

--- Engine/Graphic/Core/Stream/Stream.cpp
+++ Engine/Graphic/Core/Stream/Stream.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Gaocheng Ran <rangaocheng@outlook.com>
+// Copyright 2024 Gaocheng Ran <rangaocheng@outlook.com>
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
